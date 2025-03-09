5c24c9711e4333e41c00d64d6d645fdaf197a1f6
Ashwin Nair <ashwinnair2011@gmail.com>
2025-03-09 23:34:25 +1100

    Enhance Swagger and JWT configuration with advanced security and UI settings

--- Program.cs
+++ Program.cs
@@ -4,6 +4,8 @@ using Microsoft.IdentityModel.Tokens;
 using shopping_list_api.Models;
 using shopping_list_api.Services;
 using System.Text;
+using Microsoft.OpenApi.Models;
+using Microsoft.OpenApi.Any;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -11,7 +13,47 @@ var builder = WebApplication.CreateBuilder(args);
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+    {
+        In = ParameterLocation.Header,
+        Description = "Please insert JWT with Bearer into field",
+        Name = "Authorization",
+        Type = SecuritySchemeType.ApiKey
+    });
+    c.AddSecurityRequirement(new OpenApiSecurityRequirement {
+        {
+            new OpenApiSecurityScheme
+            {
+                Reference = new OpenApiReference
+                {
+                    Type = ReferenceType.SecurityScheme,
+                    Id = "Bearer"
+                }
+            },
+            new string[] { }
+        }
+    });
+
+    c.TagActionsBy(api =>
+    {
+        if (api.GroupName != null)
+        {
+            return new[] { api.GroupName };
+        }
+
+        var controllerActionDescriptor = api.ActionDescriptor as Microsoft.AspNetCore.Mvc.Controllers.ControllerActionDescriptor;
+        if (controllerActionDescriptor != null)
+        {
+            return new[] { controllerActionDescriptor.ControllerName };
+        }
+
+        throw new InvalidOperationException("Unable to determine tag for endpoint.");
+    });
+    c.DocInclusionPredicate((name, api) => true);
+    c.OperationFilter<SwaggerDefaultValues>();
+});
 
 // Add DbContext
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
@@ -35,7 +77,23 @@ builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
             ValidateIssuerSigningKey = true,
             ValidIssuer = builder.Configuration["Jwt:Issuer"],
             ValidAudience = builder.Configuration["Jwt:Audience"],
-            IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Subject"]))
+            IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Key"])),
+            ClockSkew = TimeSpan.Zero
+        };
+        options.Events = new JwtBearerEvents()
+        {
+            OnAuthenticationFailed = context =>
+            {
+                if (context.Exception.GetType() == typeof(SecurityTokenExpiredException))
+                {
+                    context.NoResult();
+                    context.Response.StatusCode = 401;
+                    context.Response.ContentType = "application/json";
+                    var result = System.Text.Json.JsonSerializer.Serialize(new { success = false, result = "TOKEN_EXPIRED" });
+                    return context.Response.WriteAsync(result);
+                }
+                return Task.CompletedTask;
+            }
         };
     });
 
@@ -51,7 +109,21 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseSwaggerUI(options =>
+    {
+        options.DefaultModelsExpandDepth(-1);
+        options.DocExpansion(Swashbuckle.AspNetCore.SwaggerUI.DocExpansion.List);
+        options.EnableTryItOutByDefault();
+        options.DisplayRequestDuration();
+        options.EnablePersistAuthorization();
+        options.ConfigObject.AdditionalItems["syntaxHighlight"] = new Dictionary<string, object>
+        {
+            ["activated"] = true
+        };
+        options.ConfigObject.AdditionalItems["requestSnippetsEnabled"] = true;
+        options.ConfigObject.DefaultModelRendering = Swashbuckle.AspNetCore.SwaggerUI.ModelRendering.Model;
+        options.ConfigObject.ShowCommonExtensions = true;
+    });
 }
 
 app.UseHttpsRedirection();
@@ -63,3 +135,20 @@ app.UseAuthorization();
 app.MapControllers();
 
 app.Run();
+
+// Move the SwaggerDefaultValues class here, after all top-level statements
+public class SwaggerDefaultValues : Swashbuckle.AspNetCore.SwaggerGen.IOperationFilter
+{
+    public void Apply(OpenApiOperation operation, Swashbuckle.AspNetCore.SwaggerGen.OperationFilterContext context)
+    {
+        if (context.ApiDescription.RelativePath.Contains("login") && context.ApiDescription.HttpMethod.Equals("POST", StringComparison.OrdinalIgnoreCase))
+        {
+            var schema = operation.RequestBody.Content["application/json"].Schema;
+            schema.Example = new OpenApiObject
+            {
+                ["userName"] = new OpenApiString("system.user"),
+                ["password"] = new OpenApiString("system.user")
+            };
+        }
+    }
+}
